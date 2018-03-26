94d34919ebf7ebcb24e35847efeb3adbe70ebc16
jmrein <jr27@sanger.ac.uk>
2018-03-26 16:02:24 +0100

    Add some more tests

--- test.pl
+++ test.pl
@@ -2,8 +2,9 @@
 use strict;
 use warnings;
 use Calculator qw/add/;
-use Test::More tests => 3;
+use Test::More tests => 4;
 
-is( add(42),        42, 'Handles a single number' );
-is( add(qw/5 2 3/), 10, 'Adds 3 numbers' );
-is( add( 1 .. 6 ),  21, 'Adds a sequence' );
+is( add(42),          42, 'Handles a single number' );
+is( add(qw/5 2 3/),   10, 'Adds 3 numbers' );
+is( add( 1 .. 6 ),    21, 'Adds a sequence' );
+is( add(qw/5 2 -16/), -9, 'Handles negatives' );
