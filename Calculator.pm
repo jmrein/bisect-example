787577359bd14406ca0ba16a1a7577089439f7e4
jmrein <jr27@sanger.ac.uk>
2018-03-26 16:15:13 +0100

    Add a fancy new subtract feature

--- Calculator.pm
+++ Calculator.pm
@@ -2,13 +2,22 @@ package Calculator;
 use strict;
 use warnings;
 use base qw/Exporter/;
-our @EXPORT_OK = qw/add/;
+our @EXPORT_OK = qw/add subtract/;
 
 sub add {
     my @values = @_;
     my $sum    = 0;
     foreach (@values) {
         $sum += $_;
+    }
+    return $sum;
+}
+
+sub subtract {
+    my ( $first, @values ) = @_;
+    my $sum = $first;
+    foreach (@values) {
+        $sum -= $_;
     }
     return $sum;
 }
